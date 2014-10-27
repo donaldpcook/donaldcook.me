8c7058d6c40b232b5b05d777116c23e0e203e897
Donald Cook <Donald.Cook@rga.com>
2014-10-27 09:08:21 -0500

    animation updates

--- app/js/animation.js
+++ app/js/animation.js
@@ -1,13 +1,12 @@
 (function() {
-  document.body.querySelector('.samples-thumbs').addEventListener('click', function() {
+  document.body.querySelector('.js-thumbs').addEventListener('click', function() {
     document.body.querySelector('.samples-full').className += ' animateIn';
-
-    //document.body.querySelector('.samples-full').className = document.body.querySelector('.samples-full').className.replace(/(?:^|\s)is-hidden(?!\S)/g , '');
   }, false);
 
-  //document.body.querySelector('.samples-full').addEventListener('webkitAnimationEnd', function() {
-    //debugger;
-  //});
+  document.body.querySelector('.js-close').addEventListener('click', function(evt) {
+    var animateEl = evt.currentTarget.parentNode.parentNode.parentNode;
+    animateEl.className = animateEl.className.replace(/(?:^|\s)animateIn(?!\S)/g, '');
+  });
 
 }());
 
